62323a4296f7bda1a7b0ce87f81b4efb8e5de8fc
Dominik <d-kutscha@outlook.de>
2024-03-06 23:56:45 +0100

    timer 1 - 6 seconds imlpemented. random guess on board implemented

--- main.c
+++ main.c
@@ -23,6 +23,37 @@ int enemy_board[10][10];
 int my_board[10][10];
 
 
+int modulo(uint8_t zaehler, int nenner) {
+    while (zaehler >= nenner) {
+        zaehler -= nenner;
+    }
+    return zaehler;
+}
+
+//timer method, which will be executed
+void Interrupt8_Handler(void){
+  timer_clearCompareEvent();
+  
+  uint8_t randomNumber1 = rng_getRandomValue_waiting();
+  uint8_t randomNumber2 = rng_getRandomValue_waiting();
+
+  //uart_writeByte(modulo(randomNumber1, 10) + 48);
+  //uart_writeByte(modulo(randomNumber2, 10) + 48);
+
+  //my_board[modulo(randomNumber1, 10)][modulo(randomNumber2, 10)] = 49;
+  my_board[0][0] = modulo(randomNumber2, 10) + 48;
+  //my_board[0][0] = 49;
+  //uart_writeByte(my_board[0][0]);
+  //timer_init_detailed(5, 30, 19530);
+  draw_screen(&my_board, &enemy_board);
+
+  int time_offset = 3730;
+  int new_time_delay = modulo(rng_getRandomValue_waiting(), 16) * 1000;
+  time_offset += new_time_delay;
+  timer_init_detailed(28, 30, time_offset);
+
+}
+
 
 void setup_board(int board[10][10]){
     for(int i = 0; i < 10; i++) {
@@ -39,7 +70,7 @@ void execute_ANSI_esc_codes(char esc_code[]){
     }
 }
 
-void draw_screen(int my_board[10][10], int enemy_board[10][10]){
+void draw_screen(int* my_board[10][10], int* enemy_board[10][10]){
   
 
   draw_board(my_board, "\033[3;5H");
@@ -87,18 +118,31 @@ void draw_board(int board[10][10], char start_pos[]){
   }
 }
 
+
 int main( void )
 {
   uart_init();
   rng_init();
+  
+  // 5 seconds
+  //timer_init_detailed(28, 30, 19730);
 
-  //timer_init_detailed(15, 3, 1953);
+  // 4 seconds
+  //timer_init_detailed(28, 30, 15730);
+
+  // 3 seconds
+  timer_init_detailed(28, 30, 11730);
+
+  // 2 seconds
+  //timer_init_detailed(28, 30, 7730);
+
+  // 1 second
+  //timer_init_detailed(28, 30, 3730);
+
+  // range is between 16.000
 
   int input_user[2]; 
   int input_counter = 0;
-
-  int enemy_board[10][10]; 
-  int my_board[10][10];
 
   for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
@@ -111,7 +155,7 @@ int main( void )
   //setup_board(enemy_board);
 
 
-  draw_screen(my_board, enemy_board);
+  draw_screen(&my_board, &enemy_board);
   execute_ANSI_esc_codes("\033[15;2H");
   //clean_board(my_board);
 
@@ -130,7 +174,6 @@ int main( void )
       uint8_t value = rng_getRandomValue_immediately();
     } while (value < 48 && value > 60);
     */
-
     if(0 != read_char){
       
       //write_board_on_screen(enemy_board, 0);
@@ -165,8 +208,9 @@ int main( void )
 
             execute_ANSI_esc_codes(clear_screen);
             execute_ANSI_esc_codes(set_pos);
-            draw_screen(my_board, enemy_board);
+            draw_screen(&my_board, &enemy_board);
             execute_ANSI_esc_codes("\033[15;2H");
+            
           }
       }
 
